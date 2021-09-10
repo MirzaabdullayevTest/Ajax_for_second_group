1de9e5885e24eab0317020d5bd98921d05928f6c
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2021-09-10 18:52:13 +0500

    fetch request

--- index.js
+++ index.js
@@ -2,49 +2,28 @@ window.addEventListener('load', function (e) {
     const url = 'https://jsonplaceholder.typicode.com/users'
 
     function sendXHR(method, url, body) {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest() // funksiya global
-
-            /* GET => bu serverdan nimanidur olish ko'rish */
-            /* POST => bu nimanidur yaratish yoki server ga jo'natish */
-            xhr.open(method, url)
-
-            xhr.setRequestHeader('content-type', 'application/json')
-
-            xhr.responseType = 'json' // massivga aylantirdik
-
-            xhr.onload = function () {
-                // console.log(typeof xhr.response);  // string // json // 
-                // console.log(xhr.response);
-                // let asd = JSON.parse(xhr.response)
-                if (xhr.status < 400) {
-                    resolve(xhr.response);
-                } else {
-                    reject('Xato bor ' + xhr.status);
-                }
+        // default method GET
+        return fetch(url, {
+            method,
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(body)
+        }).then(res => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                console.error('This is error', res.status);
             }
-
-            xhr.send(JSON.stringify(body))
+        }).catch((err) => {
+            console.log(err);
         })
     }
 
-    const names = document.querySelector('.names');
-
-    // sendXHR('GET', url).then((res) => {
-    //     console.log(res);
-    //     res.map((val) => {
-    //         let name = document.createElement('div')
-    //         name.innerHTML = val.name
-    //         names.appendChild(name)
+    // sendXHR('GET', url).then(data => {
+    //     data.map((val, index) => {
+    //         console.log(val.name);
     //     })
-    // }).catch((err) => {
+    // }).catch(err => {
     //     console.log(err);
-    // }).finally(() => {
-    //     console.log('Finnally');
-    // })
-
-    // sendXHR('GET', 'https://jsonplaceholder.typicode.com/comments').then((res) => {
-    //     console.log(res);
     // })
 
     const user = {
@@ -55,10 +34,34 @@ window.addEventListener('load', function (e) {
 
     sendXHR("POST", url, user).then((res) => {
         console.log(res);
-        names.innerHTML = res.id + res.name
     }).catch((err) => {
         console.log(err);
     })
-
-
 });
+
+
+
+ // return new Promise((resolve, reject) => {
+    //     const xhr = new XMLHttpRequest() // funksiya global
+
+    //     /* GET => bu serverdan nimanidur olish ko'rish */
+    //     /* POST => bu nimanidur yaratish yoki server ga jo'natish */
+    //     xhr.open(method, url)
+
+    //     xhr.setRequestHeader('content-type', 'application/json')
+
+    //     xhr.responseType = 'json' // massivga aylantirdik
+
+    //     xhr.onload = function () {
+    //         // console.log(typeof xhr.response);  // string // json // 
+    //         // console.log(xhr.response);
+    //         // let asd = JSON.parse(xhr.response)
+    //         if (xhr.status < 400) {
+    //             resolve(xhr.response);
+    //         } else {
+    //             reject('Xato bor ' + xhr.status);
+    //         }
+    //     }
+
+    //     xhr.send(JSON.stringify(body))
+    // })
